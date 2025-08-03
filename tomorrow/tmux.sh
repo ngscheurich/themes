a0eb18bc2fc4e1fca6b1b527979386b57203afc7
N. G. Scheurich <nick@scheurich.me>
2025-08-03 18:40:03 -0500

    feat: tweak tomorrow tmux colors

--- tomorrow/tmux.sh
+++ tomorrow/tmux.sh
@@ -8,23 +8,23 @@ set -euo pipefail
 SCRIPT="${XDS_DATA_HOME:-$HOME/.local/share}/grimoire/tmux_statusline.sh"
 
 if [ -x "$SCRIPT" ]; then
-  export TMUX_COLOR_MAIN_FG=colour7
-  export TMUX_COLOR_MAIN_BG=#b5b7b4
+  export TMUX_COLOR_MAIN_FG=colour8
+  export TMUX_COLOR_MAIN_BG=#c4c8c6
 
   export TMUX_COLOR_LEFT_FG=#e9e9e8
   export TMUX_COLOR_LEFT_BG=colour2
 
-  export TMUX_COLOR_ACTIVE_FG=colour7
+  export TMUX_COLOR_ACTIVE_FG=colour8
   export TMUX_COLOR_ACTIVE_BG=#e9e9e8
 
-  export TMUX_COLOR_SEG_1_FG=colour7
-  export TMUX_COLOR_SEG_1_BG=#b5b7b4
+  export TMUX_COLOR_SEG_1_FG=colour8
+  export TMUX_COLOR_SEG_1_BG=#c4c8c6
 
   export TMUX_COLOR_SEG_2_FG=#e9e9e8
-  export TMUX_COLOR_SEG_2_BG=colour7
+  export TMUX_COLOR_SEG_2_BG=colour8
 
   export TMUX_COLOR_SEG_3_FG=#e9e9e8
-  export TMUX_COLOR_SEG_3_BG=colour7
+  export TMUX_COLOR_SEG_3_BG=colour8
 
   "$SCRIPT"
 else
