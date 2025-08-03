080fe3cae4fc05896a3ba1fead9639478928914d
N. G. Scheurich <nick@scheurich.me>
2025-08-03 18:39:31 -0500

    feat: add template (catppuccin mocha)

---
+++ __template__/nvim/lua/ngs/theme.lua
@@ -0,0 +1,50 @@
+-- Based on Catppuccin Mocha (https://catppuccin.com/palette/)
+
+local palette = {
+  -- stylua: ignore start
+  rosewater = "#f5e0dc",
+  flamingo  = "#f2cdcd",
+  pink      = "#f5c2e7",
+  mauve     = "#cba6f7",
+  red       = "#f38b8a",
+  maroon    = "#eba0ac",
+  peach     = "#fab387",
+  yellow    = "#f2e2af",
+  green     = "#a6e3a1",
+  teal      = "#94e2d5",
+  sky       = "#89dceb",
+  sapphire  = "#74c7e7",
+  blue      = "#89b4fa",
+  lavender  = "#b4befe",
+  text      = "#cdd6f4",
+  subtext1  = "#bac2de",
+  subtext0  = "#a6adc8",
+  overlay2  = "#9399b2",
+  overlay1  = "#7f849c",
+  overlay0  = "#6c7086",
+  surface2  = "#585b70",
+  surface1  = "#45475a",
+  surface0  = "#313244",
+  base      = "#1e1e2e",
+  mantle    = "#181825",
+  crust     = "#11111b",
+  -- stylua: ignore end
+}
+
+return {
+  name = "cortado",
+  colorscheme = "catppuccin",
+
+  apply = function(setup)
+    setup({
+      color_overrides = { mocha = palette },
+      integrations = { aerial = true, blink_cmp = true },
+    })
+
+    vim.cmd.colorscheme("catppuccin-mocha")
+    -- vim.cmd.hi("FloatBorder guifg=" .. palette.surface2)
+    -- vim.cmd.hi("SnacksPicker guibg=" .. palette.base)
+    -- vim.cmd.hi(string.format("SnacksPickerBorder guifg=%s guibg=%s", palette.surface2, palette.base))
+    -- vim.cmd.hi("NormalAlt guibg=" .. palette.mantle)
+  end,
+}
---
+++ __template__/tmux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Sets colors for my tmux statusline, then reloads it.
+#
+
+set -euo pipefail
+
+SCRIPT="${XDS_DATA_HOME:-$HOME/.local/share}/grimoire/tmux_statusline.sh"
+
+if [ -x "$SCRIPT" ]; then
+  export TMUX_COLOR_LEFT_FG=colour0
+  export TMUX_COLOR_LEFT_BG=colour2
+
+  export TMUX_COLOR_MAIN_FG=colour15
+  export TMUX_COLOR_MAIN_BG=colour8
+
+  export TMUX_COLOR_ACTIVE_FG=colour15
+  export TMUX_COLOR_ACTIVE_BG=colour0
+
+  export TMUX_COLOR_SEG_1_FG=colour15
+  export TMUX_COLOR_SEG_1_BG=colour8
+
+  export TMUX_COLOR_SEG_2_FG=colour0
+  export TMUX_COLOR_SEG_2_BG=colour7
+
+  export TMUX_COLOR_SEG_3_FG=colour0
+  export TMUX_COLOR_SEG_3_BG=colour7
+
+  "$SCRIPT"
+else
+  echo "📖 No viable script in sight... ($SCRIPT)"
+  exit 1
+fi
